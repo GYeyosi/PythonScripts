41e99be7b360655742ed4dd30354293999188fdc
root <root@localhost.localdomain>
2017-04-11 17:19:38 +0530

    change time and added sample output

--- script.py
+++ script.py
@@ -23,9 +23,10 @@ for i in range(1,12):
 		for joke in jokes:
 			j=joke.get_text()
 			dictionary[k] = j
-			k+=1
 			with open(filename,"w") as jokes:
 				json.dump(dictionary, jokes, ensure_ascii=False,indent=4)
-			time.sleep(1)
-	print ("done"+str(i))
+			print( str(k) +"th joke is into the file jokes.json")
+			k+=1
+			time.sleep(20)
+	print ("Done with jokes in Page"+str(i))
 	
