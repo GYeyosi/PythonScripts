83a9af5bdb8c7e448f1634612b5f0dcd22f72b0b
root <root@localhost.localdomain>
2017-04-11 16:57:21 +0530

    updated script.py

--- script.py
+++ script.py
@@ -3,26 +3,29 @@
 import requests 
 import time
 from bs4 import BeautifulSoup
+import json
+
 
 filename = "jokes.json"	
-
+dictionary = {}
+k=1
 for i in range(1,12):
 	url="http://123hindijokes.com/very-funny-jokes/"+str(i)
+	
 	source=requests.get(url)
-
+	
 	source=BeautifulSoup(source.content,"lxml")
 
 	containers = source.find_all('ul',{'class':'statusList'})
-	f = open(filename,"a")
+	
 	for container in containers:
 		jokes=container.find_all("li")
 		for joke in jokes:
-			j=str(joke).replace('<li>','')
-			j=str(j).replace('</li>','')
-			j=str(j).replace('<br/>','')
-			j=str(j).replace('<p>','')
-			j=str(j).replace('</p>','')
-			f.write(str(j)+'\n'+'\n')
-
-	f.close()
-	print "done"
+			j=joke.get_text()
+			dictionary[k] = j
+			k+=1
+			with open(filename,"w") as jokes:
+				json.dump(dictionary, jokes, ensure_ascii=False,indent=4)
+			time.sleep(1)
+	print ("done"+str(i))
+	
